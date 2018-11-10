5e42ea0c4bbdc676131c4e6a944b2dd2dd9afa59
魏健 <1719003878@qq.com>
2018-11-10 16:52:58 +0800

    json模板

--- app/src/main/java/com/example/a69477/myapplication/TestFragment.java
+++ app/src/main/java/com/example/a69477/myapplication/TestFragment.java
@@ -4,17 +4,21 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class TestFragment extends Fragment {
 
     private TextView tv,tv_title;
+    private TextView title;
 
     private String type;
     public static TestFragment newInstance(String name) {
@@ -49,11 +53,14 @@ public class TestFragment extends Fragment {
 
     /**
      *
-     * @param json 直接通过json传参，方便
+     * @param json 直接通过json传参，方便geguia buddle不能存jsonobject
      * @return
      */
     public static TestFragment newInstance(JSONObject json){
         TestFragment fragment=new TestFragment();
+        Bundle args=new Bundle();
+        args.putString("JSON_STRING",json.toString());
+        fragment.setArguments(args);
         return fragment;
     }
 
@@ -69,30 +76,53 @@ public class TestFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        /*tv = (TextView) view.findViewById(R.id.fragment_test_tv);
-        tv_title=(TextView)view.findViewById(R.id.fragment_test_title);
-        Bundle bundle = getArguments();
-        if (bundle != null) {
-            if(!bundle.get("type").equals("")){//已标记分类
-                String title=bundle.get("title").toString();
-                String body = bundle.get("body").toString();
+        Bundle bundle= getArguments();
+        JSONObject json=new JSONObject();
+        title=view.findViewById(R.id.title);
+        if(bundle.getString("JSON_STRING")!=null) {
+            try {
+                String json_string = bundle.getString("JSON_STRING");
+                json = new JSONObject(json_string);
+                title.setText(json.get("title").toString());
+                title.setVisibility(View.VISIBLE);
+                for (int i = 1; i <= 5; i++) {   //假设最多5个part
+                    if (json.optJSONObject("part" + i) != null) {     //存在某个part
+                        JSONObject temp = json.optJSONObject("part" + i);//这个part
+                        String number = temp.optString("number");
 
-                tv_title.setText(title);
-                tv_title.setVisibility(View.VISIBLE);
-                tv_title.setTextSize(35);
+                        String strTitle = "title" + number;
+                        int title_id = getResources().getIdentifier(strTitle, "id", "com.example.a69477.myapplication");
+                        TextView title = view.findViewById(title_id);
+                        title.setText(temp.optString("title"));
+                        title.setVisibility(View.VISIBLE);
 
-                tv.setText(body);
-                tv.setTextSize(18);
-                tv.setTypeface(Typeface.SERIF);
+                        String strTitleIntro = strTitle + "_intro";
+                        int title_intro_id = getResources().getIdentifier(strTitleIntro, "id", "com.example.a69477.myapplication");
+                        TextView title_intro = view.findViewById(title_intro_id);
+                        title_intro.setText(temp.optString("intro"));
+                        title_intro.setVisibility(View.VISIBLE);
 
+                        JSONArray data = temp.optJSONArray("data");
+                        int length = data.length();
+                        for (int j = 1; j <= length; j++) {
+                            String tempData = data.optString(j - 1);
+                            String title_data_name = "part" + number + "_" + j;
+                            int tempData_id = getResources().getIdentifier(title_data_name, "id", "com.example.a69477.myapplication");
+                            TextView tempDataTextView = view.findViewById(tempData_id);
+                            tempDataTextView.setText(tempData);
+                            tempDataTextView.setVisibility(View.VISIBLE);
+                        }
+
+                    }
+                }
+
+            } catch (JSONException e) {
+                title.setText("Whops,something is wrong");
             }
-            else {
-                tv_title.setVisibility(View.GONE);
-                String name = bundle.get("name").toString();
-                tv.setText(name);
-            }
-        }*/
-
+        }
+        else {
+            title.setText(bundle.get("name").toString());
+        }
     }
 
 }
--- app/src/main/java/com/example/a69477/myapplication/application.java
+++ app/src/main/java/com/example/a69477/myapplication/application.java
@@ -12,11 +12,14 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -73,7 +76,73 @@ public class application extends AppCompatActivity {
         viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(viewPagerAdapter);
         List<Fragment> list = new ArrayList<>();
-        list.add(TestFragment.newInstance("第一页"));
+
+        JSONObject json1=new JSONObject();//第一个界面的json数据,由part123组成
+
+        try{
+            json1.put("title","Admission Requirements");
+            JSONObject part1=new JSONObject();
+            JSONObject part2=new JSONObject();
+            JSONObject part3=new JSONObject();
+
+            part1.put("title","Eligibility");
+            part1.put("intro","To be eligible for admission to the programmes, a candidate shall satisfy both the University Entrance Requirements and the Programme Entrance Requirements including but not limited to:");
+            //part1的abc数据部分
+            JSONArray data1=new JSONArray();
+            data1.put("a.    shall comply with the General Regulations;");
+            data1.put("b.    shall hold a Bachelor’s degree of this University or a qualification of equivalent standard from this University or another comparable institution accepted for this purpose;");
+            data1.put("c.    for a candidate who is seeking admission on the basis of a qualification from a university or comparable institution outside Hong Kong of which the language of teaching and/or examination is not English, shall satisfy the University English language requirement applicable to high degrees as prescribed under General Regulation G2(b); and");
+            data1.put("d.    satisfy the examiners in a qualifying examination if required.");
+            part1.put("data",data1);
+            part1.put("number",1);
+            json1.put("part1",part1);
+
+            part2.put("title","Regulations");
+            part2.put("intro","");
+            //part2的abc数据部分
+            JSONArray data2=new JSONArray();
+            data2.put("1. General Regulations of The University\n" +
+                    "https://www4.hku.hk/pubunit/drcd/files/pgdr2017-18/genreg.pdf");
+            data2.put("2. University's Regulations for Taught Postgraduate Curricula: \n" +
+                    "https://www4.hku.hk/pubunit/drcd/files/pgdr2017-18/tpg-regulations.pdf");
+            data2.put("3. Degree Regulations of MSc(CompSc) for students admitted to the curriculum in 2017-2018 and thereafter:\n" +
+                    "http://www.msc-cs.hku.hk/Media/Default/RegulationSyllabus/Regulations_MSc_2017-18.pdf\n" +
+                    "http://www.msc-cs.hku.hk/Media/Default/RegulationSyllabus/Syllabus_MSc(CompSc)_2018-19.pdf");
+
+            part2.put("data",data2);
+            part2.put("number",2);
+            json1.put("part2",part2);
+
+
+            part3.put("title","English Language Proficiency Requirement");
+            part3.put("intro","Applicants who wish to be admitted on the basis of a qualification from a university or comparable institution outside Hong Kong where the language of teaching and/or examination is not English are required to obtain:");
+            JSONArray data3=new JSONArray();
+            data3.put("a. a score of 550 or above (paper-based test) or 80 or above (internet-based test) in the Test of English as a Foreign Language (TOEFL).  Applicants who took the TOEFL should request the Educational Testing Service (ETS) to send an official score report to the University directly. For this purpose, the HKU TOEFL code is 9671. Test scores more than two years old will not be accepted; or");
+            data3.put("b. a minimum overall band of 6 with no subtest lower than 5.5 in the International English Language Testing System (IELTS); or");
+            data3.put("c. grade C or above in the Overseas General Certificate of Education (GCE); or");
+            data3.put("d. grade C or above in the International General Certificate of Secondary Education (IGCSE); or");
+            data3.put("e. grade C or above in the Cambridge Test of Proficiency in English Language;");
+            part3.put("data",data3);
+            part3.put("number",3);
+            json1.put("part3",part3);
+
+            list.add(TestFragment.newInstance(json1));
+        }
+        catch (JSONException e){
+            list.add(TestFragment.newInstance("Whops, something is wrong!"));
+        }
+
+
+
+        JSONObject json2=new JSONObject();
+        try{
+            json2.put("","");
+        }
+        catch (JSONException e){
+            list.add(TestFragment.newInstance("Whops, something is wrong!"));
+        }
+
+
         list.add(TestFragment.newInstance("第二页"));
         list.add(TestFragment.newInstance("大学"));
         list.add(TestFragment.newInstance("很难"));
