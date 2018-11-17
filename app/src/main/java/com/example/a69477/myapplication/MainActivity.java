a4b3d1b8cfc2563e56765919354b64dd8cf5ce37
魏健 <1719003878@qq.com>
2018-11-17 15:49:17 +0800

    111
    222

--- app/src/main/java/com/example/a69477/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a69477/myapplication/MainActivity.java
@@ -126,9 +126,7 @@ public class MainActivity extends AppCompatActivity  {
                     startActivityForResult(intent, 11);
                     overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
                 } else if (id == R.id.nav_Resource) {
-                    Intent intent = new Intent(MainActivity.this, Sresource.class);
-                    startActivityForResult(intent, 11);
-                    overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                    
                 }
 
                 mDrawerLayout.closeDrawers();
