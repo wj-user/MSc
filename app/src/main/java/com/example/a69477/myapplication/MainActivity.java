4a8c29e4adf70d722b346a0a43cc51d781920afc
魏健 <1719003878@qq.com>
2018-11-17 17:07:40 +0800

    111
    111

--- app/src/main/java/com/example/a69477/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a69477/myapplication/MainActivity.java
@@ -126,7 +126,9 @@ public class MainActivity extends AppCompatActivity  {
                     startActivityForResult(intent, 11);
                     overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
                 } else if (id == R.id.nav_Resource) {
-
+                    Intent intent = new Intent(MainActivity.this, UsefulLinks.class);
+                    startActivityForResult(intent, 11);
+                    overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
                 }
 
                 mDrawerLayout.closeDrawers();
--- app/src/main/java/com/example/a69477/myapplication/UsefulLinks.java
+++ app/src/main/java/com/example/a69477/myapplication/UsefulLinks.java
@@ -21,9 +21,31 @@ public class UsefulLinks extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         ImageView myheader = (ImageView) findViewById(R.id.img_fe);
-        Glide.with(this).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/GraduatesAlumni.jpg").into(myheader);
+        Glide.with(this).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/UsefulLinks.jpg").into(myheader);
 
-
+        TextView useful_intro=findViewById(R.id.useful_intro);
+        useful_intro.setText("The University of Hong Kong Home Page\n" +
+                "   -  https://www.hku.hk\n" +
+                "\n" +
+                "The Faculty of Engineering\n" +
+                "   -  http://engg.hku.hk\n" +
+                "\n" +
+                "Department of Computer Science\n" +
+                "   -  http://www.cs.hku.hk \n" +
+                "\n" +
+                "Information Technology Services\n" +
+                "   -  http://www.its.hku.hk\n" +
+                "\n" +
+                "Centre of Development and Resources for Students\n" +
+                "   -  http://www.cedars.hku.hk   \n" +
+                "\n" +
+                "China Affairs Office\n" +
+                "   -  http://www.aal.hku.hk/cao\n" +
+                "\n" +
+                "The HKU Libraries\n" +
+                "   -  https://lib.hku.hk/\n" +
+                "\n" +
+                "Please email msccs@cs.hku.hk if you find any above links broken or if you have any suggestion of other useful links for us to include in the list.");
 
 
         mytoolBar.setNavigationOnClickListener(new View.OnClickListener() {
